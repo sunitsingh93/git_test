05fcef6dde1f10454babb54481c5a00d3e721f08
Sunit Singh <ssingh3@sciformix.com>
2019-08-17 14:44:19 +0530

    test 2 commiting

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def addone(num):
-    return num + 1
+    return num + 3
 
 if __name__ == "__main__":
     print(addone(3))
---
+++ test_2.py
@@ -0,0 +1,5 @@
+def addtwo(num):
+    return (num + 10)
+
+if __name__ == "__main__":
+    print(addtwo(3))
